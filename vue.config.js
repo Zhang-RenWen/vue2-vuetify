02a7582b022c6b145d4346ff5af80718b5500b8c
ren-wen <zhangrenwen0107@gmail.com>
2022-09-20 14:19:33 +0800

    package.json setting

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,33 @@
-const path = require("path");
-const Timestamp = new Date().getTime();
-const name = "EUISV";
-const port = 80;
+const path = require('path')
+const Timestamp = new Date().getTime()
+const name = 'EUISV'
+const port = 80
 const resolve = (dir) => {
-  return path.join(__dirname, dir);
-};
+  return path.join(__dirname, dir)
+}
 const devServer = {
-  port: port,
-};
-if (process.env.VUE_APP_ENV === "development") {
+  port
+}
+if (process.env.VUE_APP_ENV === 'development') {
   devServer.proxy = {
-    "/euisbq": {
+    '/euisbq': {
       target: `${process.env.VUE_APP_CAS_REDIRECT_URI} :${process.env.VUE_APP_APIPORT_EUISBQ}`,
       ws: true,
       changeOrigin: true,
-      pathRewrite: { "^/euisbq": "" },
+      pathRewrite: { '^/euisbq': '' }
     },
-    "/euisb": {
+    '/euisb': {
       target: `${process.env.VUE_APP_CAS_REDIRECT_URI} :${process.env.VUE_APP_APIPORT_EUISB}`,
       ws: true,
       changeOrigin: true,
-      pathRewrite: { "^/euisb": "" },
-    },
-  };
+      pathRewrite: { '^/euisb': '' }
+    }
+  }
 }
 
 module.exports = {
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
-  transpileDependencies: ["vuetify"], // vuetify setting
+  transpileDependencies: ['vuetify'], // vuetify setting
   runtimeCompiler: true,
   parallel: true,
   productionSourceMap: false,
@@ -35,30 +35,30 @@ module.exports = {
     name,
     output: {
       filename: `[name].js?v=${Timestamp}`,
-      chunkFilename: `[name].js?v=${Timestamp}`,
-    },
+      chunkFilename: `[name].js?v=${Timestamp}`
+    }
   },
   devServer,
   chainWebpack(config) {
-    //設定 svg-sprite-loader
-    config.module.rule("svg").exclude.add(resolve("src/assets/svg")).end();
+    // 設定 svg-sprite-loader
+    config.module.rule('svg').exclude.add(resolve('src/assets/svg')).end()
     config.module
-      .rule("icons")
+      .rule('icons')
       .test(/\.svg$/)
-      .include.add(resolve("src/assets/svg"))
+      .include.add(resolve('src/assets/svg'))
       .end()
-      .use("svg-sprite-loader")
-      .loader("svg-sprite-loader")
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
       .options({
-        symbolId: "icon-[name]",
+        symbolId: 'icon-[name]',
         limit: 2000,
-        esModule: false,
+        esModule: false
       })
-      .end();
+      .end()
     config.module
-      .rule("pdf")
+      .rule('pdf')
       .test(/l.pdf$/)
-      .use("file-loader")
-      .loader("file-loader");
-  },
-};
+      .use('file-loader')
+      .loader('file-loader')
+  }
+}
