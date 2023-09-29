2bd2287b787400ea1ea20009afbad0c25bd8dd1b
zhangrenwen <zhangrenwen0107@gmail.com>
2023-09-29 22:11:52 +0800

    fix(vue.config.js): remove unused settings

--- vue.config.js
+++ vue.config.js
@@ -1,28 +1,8 @@
 const path = require('path')
 const Timestamp = new Date().getTime()
-const name = 'EUISV'
-const port = 80
+
 const resolve = (dir) => {
   return path.join(__dirname, dir)
-}
-const devServer = {
-  port
-}
-if (process.env.VUE_APP_ENV === 'development') {
-  devServer.proxy = {
-    '/euisbq': {
-      target: `${process.env.VUE_APP_CAS_REDIRECT_URI} :${process.env.VUE_APP_APIPORT_EUISBQ}`,
-      ws: true,
-      changeOrigin: true,
-      pathRewrite: { '^/euisbq': '' }
-    },
-    '/euisb': {
-      target: `${process.env.VUE_APP_CAS_REDIRECT_URI} :${process.env.VUE_APP_APIPORT_EUISB}`,
-      ws: true,
-      changeOrigin: true,
-      pathRewrite: { '^/euisb': '' }
-    }
-  }
 }
 
 module.exports = {
@@ -32,13 +12,11 @@ module.exports = {
   parallel: true,
   productionSourceMap: false,
   configureWebpack: {
-    name,
     output: {
       filename: `[name].js?v=${Timestamp}`,
       chunkFilename: `[name].js?v=${Timestamp}`
     }
   },
-  devServer,
   chainWebpack(config) {
     // 設定 svg-sprite-loader
     config.module.rule('svg').exclude.add(resolve('src/assets/svg')).end()
